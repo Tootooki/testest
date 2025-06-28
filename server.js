6d145fc25c46d12cf77b5a8d13146aa9522ea0e1
Tootooki <Svlad92@gmail.com>
2025-06-28 10:46:40 +0700

    Improve multi-site viewer layout

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ app.use(express.static(path.join(__dirname)));
 app.get('/proxy', (req, res) => {
   const url = req.query.url;
   if (!url) return res.status(400).send('Missing url param');
-  request({ url, followRedirect: true, headers: { 'User-Agent': 'Mozilla/5.0' } })
+  request({
+    url,
+    followRedirect: true,
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122 Safari/537.36'
+    }
+  })
     .on('response', r => {
       delete r.headers['x-frame-options'];
       delete r.headers['content-security-policy'];
